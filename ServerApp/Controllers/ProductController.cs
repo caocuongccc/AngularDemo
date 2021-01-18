283b5d2c1b7f4c8c5dec39f61fa106ab2106a03a
Le Cao Cuong <lecaocuong@vnpt.vn>
2021-01-18 11:57:21 +0700

    + add

---
+++ ServerApp/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerApp.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductController : Controller
+    {
+        private readonly DataContext _dataContext;
+
+        public ProductController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        [HttpGet("{id}")]
+        public Product GetProduct(long id)
+        {
+            var prod = _dataContext.Products.Find(id);
+            return prod;
+        }
+    }
+}
---
+++ ServerApp/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ServerApp.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+
+
+    }
+}
